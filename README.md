0752ec0b15bdd5612077800b87ebfc9da609cd3f
Septs <github@septs.pw>
2020-02-02 21:39:49 +0800

    refactor: simplify code (#2)
    * refactor: simplify code
    
    * fix: pdf link building logic
    
    * modify filename
    
    * fix: add referer header to aria2c input file
    
    * feat: ignore starts with `#` in category code

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 # CMPEDUDownload
 
-机械工业出版社PDF下载器
+机械工业出版社 PDF 下载器
 
 ## 使用方法
 
 ```bash
-pip3 install requests --user
-python3 Download.py
+pip3 install requests
+# 获得 aria2 input file
+python3 download.py | tee downloads.txt
+# 使用 aria2 进行下载
+aria2c -i downloads.txt
 ```
 
 ## 感谢
 
-https://www.cnblogs.com/owasp/p/6413480.html
-
-
-
+<https://www.cnblogs.com/owasp/p/6413480.html>
---
+++ download.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# coding=utf-8
+import json
+
+import requests
+
+referer = "https://cmpebooks.s3.cn-north-1.amazonaws.com.cn/pdfReader/generic/build/pdf.worker.js"
+
+
+def get_books(code: str):
+    response = requests.post(
+        "http://ebooks.cmanuf.com/getBookCategoryInfo",
+        data={"code": code, "page": 1, "limit": 1000, "px": "desc"},
+    )
+    books = response.json()["module"]
+    for bookInfo in books:
+        cover = bookInfo["img"]
+        if len(cover) <= 55 or "cmpebooks.s3.cn-north-1.amazonaws.com.cn" not in cover:
+            continue
+        pdf_link = (
+            cover.replace("/Cover/", "/PDF/")
+            .replace("Cover1.jpg", "2.pdf")
+            .replace("Cover2.jpg", "2.pdf")
+            .replace("cover_front_L.jpg", "L.pdf")
+        )
+        filename = (
+            bookInfo["name"]
+            .strip()
+            .replace("  ", " ")
+            .replace("/", "_")
+            .replace(" ", "_")
+        )
+        yield pdf_link, "%s.pdf" % filename
+
+
+def main():
+    catgories = {}
+    with open("categories.json") as fp:
+        catgories = json.load(fp)
+    for category_code, category_name in catgories.items():
+        if category_code[0] == "#":
+            continue
+        for pdf_link, filename in get_books(category_code):
+            saved_path = "downloads/%s-%s/%s" % (category_code, category_name, filename)
+            print("%s\n\treferer=%s\n\tout=%s" % (pdf_link, referer, saved_path))
+
+
+if __name__ == "__main__":
+    main()
