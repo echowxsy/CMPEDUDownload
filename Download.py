8c98ec36737402e91a5148c731e42e6425bc5939
Echowxsy <echowxsy@gmail.com>
2020-02-02 17:33:02 +0800

    fix: catch error

--- Download.py
+++ Download.py
@@ -90,47 +90,51 @@ def downloadPdf(filename, url):
         'Referer': 'https://cmpebooks.s3.cn-north-1.amazonaws.com.cn/pdfReader/generic/build/pdf.worker.js',
         'Connection': 'keep-alive'
     }
-    filesize = int(requests.head(
-        url, headers=headers).headers['Content-Length'])
-    print("[LOG] %s filesize: %.2fMB" % (filename, filesize/(1024*1024)))
-    # 线程数
-    threadnum = 3
-    # 信号量，同时只允许3个线程运行
-    threading.BoundedSemaphore(threadnum)
-    # 默认3线程现在，也可以通过传参的方式设置线程数
-    step = filesize // threadnum
-    mtd_list = []
-    start = 0
-    end = -1
-
-    if os.path.exists(filename):
-        pass
+    try:
+        filesize = int(requests.head(
+            url, headers=headers).headers['Content-Length'])
+    except KeyError:
+        print("[ERR] error file url: %s" % (filename))
     else:
-        # 请空并生成文件
-        tempf = open(filename, 'w')
-        tempf.close()
-        # rb+ ，二进制打开，可任意位置读写
-        with open(filename, 'rb+') as f:
-            fileno = f.fileno()
-            # 如果文件大小为11字节，那就是获取文件0-10的位置的数据。如果end = 10，说明数据已经获取完了。
-            while end < filesize - 1:
-                start = end + 1
-                end = start + step - 1
-                if end > filesize:
-                    end = filesize
-                # print("start:%s, end:%s"%(start,end))
-                # 复制文件句柄
-                dup = os.dup(fileno)
-                # print(dup)
-                # 打开文件
-                fd = os.fdopen(dup, 'rb+', -1)
-                # print(fd)
-                t = MulThreadDownload(url, start, end, fd)
-                t.start()
-                mtd_list.append(t)
+        print("[LOG] %s filesize: %.2fMB" % (filename, filesize/(1024*1024)))
+        # 线程数
+        threadnum = 3
+        # 信号量，同时只允许3个线程运行
+        threading.BoundedSemaphore(threadnum)
+        # 默认3线程现在，也可以通过传参的方式设置线程数
+        step = filesize // threadnum
+        mtd_list = []
+        start = 0
+        end = -1
 
-            for i in mtd_list:
-                i.join()
+        if os.path.exists(filename):
+            pass
+        else:
+            # 请空并生成文件
+            tempf = open(filename, 'w')
+            tempf.close()
+            # rb+ ，二进制打开，可任意位置读写
+            with open(filename, 'rb+') as f:
+                fileno = f.fileno()
+                # 如果文件大小为11字节，那就是获取文件0-10的位置的数据。如果end = 10，说明数据已经获取完了。
+                while end < filesize - 1:
+                    start = end + 1
+                    end = start + step - 1
+                    if end > filesize:
+                        end = filesize
+                    # print("start:%s, end:%s"%(start,end))
+                    # 复制文件句柄
+                    dup = os.dup(fileno)
+                    # print(dup)
+                    # 打开文件
+                    fd = os.fdopen(dup, 'rb+', -1)
+                    # print(fd)
+                    t = MulThreadDownload(url, start, end, fd)
+                    t.start()
+                    mtd_list.append(t)
+
+                for i in mtd_list:
+                    i.join()
 
 
 def cleanFileName(filename):
